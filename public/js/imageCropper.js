fbe7aa090ea21e8f684b0f2cf0aab71863bdf7d7
root <jay.pandey45@gmail.com>
2019-11-17 00:59:16 +0530

    update

--- public/js/imageCropper.js
+++ public/js/imageCropper.js
@@ -1,4 +1,6 @@
 var imageCropper = false
+var saveElement = 'undefined';
+var okButton = 'undefined';
 $(document).on("change", ".image", function(){
     
     var imageReader = new FileReader();
@@ -26,23 +28,38 @@ $(document).on("change", ".image", function(){
                 initialize: function() {
                     // Active crop selection
                     this.plugins['crop'].requireFocus();
+                    saveElement = this.toolbar.element.children[3];
+                    okButton = this.toolbar.element.children[2].children[1];
                     saveEventRegister(this.toolbar.element.children[3]);
                 },  
             }
         );
     };
 });
+var okClickInterval = 'undefined';
 $(document).on('click', '#uploaded-image', function() {
-    var activeObject = imageCropper.canvas.getActiveObject();
-    $(document).find('#file_hidden').val($(document).find('#image-preview > img').attr('src'));
+    $(okButton).trigger('click');
+    if (typeof okClickInterval != 'undefined') {
+        clearTimeout(okClickInterval);
+    }
+    okClickInterval = setTimeout(function() {
+        $(saveElement).find('button').trigger('click');
+    }, 200);
+    // $(document).find('#file_hidden').val($(document).find('#image-preview > img').attr('src'));
     $('#cropperModal').modal('toggle');
 });
+
+var timeInterval = 'undefined';
 function saveEventRegister(elem) {
     $(elem).on('click', function() {
         $(document).find('#image-preview').hide();
-        setTimeout(function() {
+        if (typeof timeInterval != 'undefined') {
+            clearTimeout(timeInterval);
+        }
+        timeInterval = setTimeout(function() {
             $(document).find('#image-preview > img').addClass('img-fluid');
             $(document).find('#image-preview').show();
+            $(document).find('#file_hidden').val($(document).find('#image-preview > img').attr('src'));
         }, 100);
     });
 }
