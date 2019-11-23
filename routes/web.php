a3a5ffcc9420e7d38bc2343d74deffe8ad8b3faf
Jatin <jay.pandey45@gmail.com>
2019-11-24 00:57:17 +0530

    updaet

--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,28 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 
-//Route::get('/','HomeController@index')->name('user.home.page');
+// Authentication Routes...
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('/','FrontPageController@index');
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 
-/**
- * current affair routes
- */
 
 
- 
-Auth::routes();
+
+
+
 
 
 Route::group(['middleware' => 'auth'],function(){
