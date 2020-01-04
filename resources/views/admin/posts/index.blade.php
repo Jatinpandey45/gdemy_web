18d6869fe25377737f633dae61ee03802b125ccc
Jatin <jay.pandey45@gmail.com>
2020-01-04 22:11:57 +0530

    update

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -39,36 +39,9 @@
 
 <script src="{{asset('js/jquery-validation/dist/jquery.validate.min.js')}}"></script>
 <script src="{{asset('js/jquery-validation/dist/additional-methods.min.js')}}"></script>
-<<<<<<< HEAD
-<script type="text/javascript" src="{{asset('js/datatable.min.js')}}"></script>
-<script>
-  var table = $('#data_table_post').DataTable({
-    processing: true,
-    serverSide: true,
-    ajax: "{{route('admin.post.list.records')}}",
-    columns: [
-        {data: 'post_title', name: 'Category'},
-        // {data: 'post_desc', name: 'Description','orderable':false},
-        {data: 'month', name: 'Month','orderable' :false},
-        {data: 'publish_at', name: 'Created'},
-        {data:"action","className": "text-right", "render" : function ( data, type, row ){
-            return '<a href="'+row.edit_route+'"><i class="material-icons">edit</i></a>'+
-                '<a href="javascript:void(0);" class="remove-item" data-id="'+data+'"><i class="material-icons" data-toggle="modal" data-target="#deleteModal">delete</i></a>';
-         }
-      }
-    ]
-    });
-    $( table.table().container() )
-    .addClass( 'table table-responsive
-
-
-
-</script>
-=======
 <script src="{{asset('js/datatable.min.js')}}"></script>
 <script src="{{asset('js/post.list.js')}}"></script>
 
->>>>>>> 8e35bcccba6d13468ce2df218b03ede54d0e443d
 @endsection
 
 
