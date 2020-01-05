27e83759fd880b5f7ff3383d87277fdacb70a753
root <jay.pandey45@gmail.com>
2020-01-05 11:49:17 +0530

    Update

--- resources/views/admin/quiz/create.blade.php
+++ resources/views/admin/quiz/create.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.admin')
 
 @section('content')
-<form action="{{route('quiz.store')}}" id="post_form_id" class="row" method="POST" enctype="multipart/form-data">
-    {!! Form::open(['method' => 'POST', 'route' => ['questions.store']]) !!}
+{{-- <form action="{{route('posts.store')}}" id="post_form_id" class="row" method="POST" enctype="multipart/form-data"> --}}
+    {!! Form::open(['method' => 'POST', 'route' => ['posts.store']]) !!}
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            @lang('quickadmin.create')
+    <div class="card card-default form-group">
+        <div class="card-header h3">
+            Quiz
+            {{-- @lang('quickadmin.create') --}}
         </div>
 
-        <div class="panel-body">
+        <div class="card-body">
             <div class="row">
                 <div class="col-xs-12 form-group">
-                    {!! Form::label('topic_id', 'Topic*', ['class' => 'control-label']) !!}
+                    {!! Form::label('topic_id', 'Schedule Time*', ['class' => 'control-label']) !!}
                     {!! Form::select('topic_id', $topics, old('topic_id'), ['class' => 'form-control']) !!}
                     <p class="help-block"></p>
                     @if($errors->has('topic_id'))
@@ -22,7 +23,7 @@
                     @endif
                 </div>
             </div>
-            <div class="row">
+            {{-- <div class="row">
                 <div class="col-xs-12 form-group">
                     {!! Form::label('question_text', 'Question text*', ['class' => 'control-label']) !!}
                     {!! Form::textarea('question_text', old('question_text'), ['class' => 'form-control ', 'placeholder' => '']) !!}
@@ -141,7 +142,7 @@
                         </p>
                     @endif
                 </div>
-            </div>
+            </div> --}}
 
         </div>
     </div>
@@ -149,140 +150,6 @@
     {!! Form::submit(trans('quickadmin.save'), ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
-    <div class="col-md-9">
-        <div class="card card-default form-group">
-            <div class="card-header h3">Create Question</div>
-            <div class="card-body">
-
-                @csrf
-                <div class="form-group">
-                    <label for="post_title">Title</label>
-                    <input type="text" id="post_title" value="{{old('post_title')}}" class="form-control" placeholder="Name field must be unique" name="post_title">
-
-                    @if($errors->has('post_title'))
-                    <span class="error">{{ $errors->first('post_title') }}</span>
-                    @endif
-
-                </div>
-
-                <div class="form-group">
-                    <label for="category_slug">Slug</label>
-                    <input type="text" id="post_slug" value="{{old('post_slug')}}" class="form-control" name="post_slug">
-                    @if($errors->has('post_slug'))
-                    <span class="error">{{ $errors->first('post_slug') }}</span>
-                    @endif
-
-                </div>
-
-                <div class="form-group">
-                    <label for="post_desc">Short Description</label>
-                    <textarea class="form-control" class="validate[required]" rows="5" name="post_short_desc">{{old('post_short_desc')}}</textarea>
-                    @if($errors->has('post_short_desc'))
-                    <span class="error">{{ $errors->first('post_short_desc') }}</span>
-                    @endif
-                </div>
-
-                <div class="form-group">
-                    <label for="post_desc">Description</label>
-                    <!-- <input id="post_desc" type="hidden" name="post_desc">
-                        <trix-editor input="post_desc"></trix-editor> -->
-                    <textarea class="gk_tinymce" class="validate[required]" rows="30" name="post_desc">{{old('post_desc')}}</textarea>
-                    @if($errors->has('post_desc'))
-                    <span class="error">{{ $errors->first('post_desc') }}</span>
-                    @endif
-                </div>
-            </div>
-        </div>
-
-        <div class="card card-default form-group">
-            <div class="card-header h3">SEO Settings</div>
-            <div class="card-body">
-                <div class="form-group">
-                    <label for="post_seo_title">Title Tag</label>
-                    <input type="text" id="post_seo_title" value="{{old('post_seo_title')}}" class="form-control enable-counter" placeholder="Name field must be unique" name="post_seo_title">
-                    <div class="help-block text-count-wrapper">Most search engine use upto 70</div>
-                    @if($errors->has('post_seo_title'))
-                    <span class="error">{{ $errors->first('post_seo_title') }}</span>
-                    @endif
-
-                </div>
-
-                <div class="form-group">
-                    <label for="meta_desc">Meta Description</label>
-                    <textarea id="meta_desc" class="form-control enable-counter" name="seo_desc">{{old('seo_desc')}}</textarea>
-                    <div class="help-block">Most search engine use upto 140</div>
-                    @if($errors->has('seo_desc'))
-                    <span class="error">{{ $errors->first('seo_desc') }}</span>
-                    @endif
-
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div class="card card-default form-group ">
-            <div class="card-header h3">Publish</div>
-            <div class="card-body">
-                <div class="form-group row gk-space-between">
-                    <button type="submit" class="btn btn-success float-left">Save Draft</button>
-                    <a href="javascript:void(0)" class="btn btn-secondary float-right">Preview</a>
-                </div>
-                <div class="form-group row gk-align-center">
-                    <label for="visibility" class="col-md-4 pl-1 pr-1">Visibility</label>
-                    <select name="visibility" value="" id="visibility" class="form-control col-md-7">
-                        <option value="all">All</option>
-                        <option value="android">Android</option>
-                        <option value="ios">IOS</option>
-                        <option value="web">Web</option>
-                    </select>
-                </div>
-                <div class="form-group row gk-align-center">
-                <label for="publish_at" class="col-md-4 pl-1 pr-1">Publish</label>
-                    <div id="published_at">
-                   
-                   
-                                <input type="text" name="published_at" readonly="readonly" placeholder="Select Date.." data-input > <!-- input is mandatory -->
-
-                                <a class="input-button" title="toggle" data-toggle>
-                                    <i class="material-icons">calendar_today</i>
-                                </a>
-
-                                <a class="input-button" title="clear" data-clear>
-                                    <i class="material-icons">close</i>
-                                </a>
-                    </div>
-
-
-                    @if($errors->has('published_at'))
-                    <span class="error">{{ $errors->first('published_at') }}</span>
-                    @endif
-                </div>
-            </div>
-        </div>
-        <div class="card card-default form-group ">
-            <div class="card-header h3">Tag</div>
-            <div class="card-body">
-                {{-- <label for="tag_name">Name</label> --}}
-                {{-- <div class="form-group">
-                        <select id="tag_listing_data" name="tag_name[]" multiple="multiple"></select>
-                        <img id="loader_element_id" width="100" height="100" style="display:none;position: absolute;margin: -67px 203px -7px;" src="{{asset('images/Spinner-1s-200px.gif')}}">
-            </div> --}}
-
-
-            <div class="form-group">
-                <!-- <input type="text" class="form-control" name="tag_name" id="tag_name"> -->
-                <select id="tag_listing_data" class="form-control" name="tag_name[]" multiple="multiple"></select>
-                <img id="loader_element_id" width="100" height="100" style="display:none;position: absolute;margin: -67px 203px -7px;" src="{{asset('images/Spinner-1s-200px.gif')}}">
-            </div>
-            <div class="input-group" id="selected_post_tag">
-                <select type="hidden" class="d-none" name="post_tags" id="post_tags" value=""/>
-                </select>
-            </div>
-            
-            <input type="hidden" name="selected_id" id="selected_tag"/>
-            <input type="hidden" name="selected_name" id="selected_tag_name"/>
-        </div>
-    </div>
     {{-- <div class="card card-defaut">
                 
             </div>
@@ -293,7 +160,7 @@
             <div class="card card-defaut">
             </div> --}}
     {{-- </div> --}}
-</form>
+{{-- </form> --}}
 <!-- public/js/tiny_mce/plugins/responsivefilemanager -->
 <input type="hidden" value="{{asset('js/tiny_mce/plugins/responsivefilemanager')}}" id="filemanagerlink" />
 <input type="hidden" value="{{route('post.search.tags')}}" id="tag_search_request_route">
